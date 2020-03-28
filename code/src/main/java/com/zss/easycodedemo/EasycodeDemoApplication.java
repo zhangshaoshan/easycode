2b1498566c6fd407b7b3244ef24037fedaf2d01a
zhangshaoshan <512073236@qq.com>
2020-03-28 20:11:53 +0800

    初始化

---
+++ code/src/main/java/com/zss/easycodedemo/EasycodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zss.easycodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasycodeDemoApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/com/zss/easycodedemo/EasycodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zss.easycodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
